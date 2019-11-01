0fc2242ac37cd5522c3f766ac478ddcc43e128b1
k912867884 <k912867884@sohu.com>
2019-11-02 06:00:45 +0800

    1

--- pages/seek/seek.js
+++ pages/seek/seek.js
@@ -5,9 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    iconType: "search"
+    iconType: "search",
   },
-
+  handleadd(){
+    this.setData({
+      number:this.data.number+1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
